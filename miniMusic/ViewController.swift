366eb15f3af71abcbc1f982238acefb831eaa9d5
MarkSwiftIt <markgocharovit@gmail.com>
2022-08-09 13:12:27 +0300

    Initial Commit

---
+++ miniMusic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  miniMusic
+//
+//  Created by Mark Goncharov on 09.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
